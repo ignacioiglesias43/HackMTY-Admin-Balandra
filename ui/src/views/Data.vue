570163562ba51d59a019f82fcef8e807c1414bd7
Chaidez13 <56051074+Chaidez13@users.noreply.github.com>
2020-08-30 04:27:45 -0600

    Ticket View added
    And Light based open states from places

---
+++ ui/src/views/Data.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+      <Ticket />
+  </v-container>
+</template>
+
+<script>
+import Ticket from '../components/organisms/Ticket'
+export default {
+    name: 'Data',
+    components:{
+        Ticket
+    }
+}
+</script>
+
+<style>
+
+</style>
